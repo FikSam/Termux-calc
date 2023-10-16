8b4e026825f6f0f020aa8c8897bcd8f1ebf7fc52
FikSam <86853932+FikSam@users.noreply.github.com>
2023-10-16 11:11:09 +0200

    Add files via upload

--- hello2.py
+++ hello2.py
@@ -2,13 +2,6 @@ print("= Termux Calculator =")
 
 var1 = float(input("var1: "))  
 var2 = float(input("var2: "))  
-operation = input("+, - :")
 
-if operation == "+":
-  result = var1 + var2
-  print(result)
-elif operation == "-":
-  result = var1 - var2
-  print(result)
-else:
-  print("Invalid operation. Please enter '+' or '-' for addition or subtraction.")
+results = var1 += var2
+print(results)
