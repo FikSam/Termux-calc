3f671fb8a8507a4d3052b869258be2ef42ff3c07
FikSam <86853932+FikSam@users.noreply.github.com>
2023-10-16 10:45:22 +0200

    Add files via upload

---
+++ hello2.py
@@ -0,0 +1,15 @@
+
+print("Termux Calculator =•=")
+
+var1 = float(input("var1: "))
+var2 = float(input("var2: ")) 
+operation = input("Enter + or -: ") 
+
+if operation == "+":
+  result = var1 + var2
+  print("Результат:", result)
+elif operation == "-":
+  result = var1 - var2
+  print("Результат:", result)
+else:
+  print("Неподдерживаемая операция. Поддерживаются только + и -.")
